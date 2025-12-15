c842008480a38177df5d25363f0a85cf30faf7c7
tester2021test <69193224+tester2021test@users.noreply.github.com>
2025-12-15 15:07:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This ensures assets load correctly at the root domain
+  base: '/party-fund/', // This ensures assets load correctly at the root domain
 })
